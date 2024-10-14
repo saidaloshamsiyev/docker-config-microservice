00782d28dccc3088f5579aabea7d5b0eea32d944
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-14 09:03:13 +0500

    Initial commit

---
+++ src/main/java/org/example/dockerconfigmicroservice/DockerConfigMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package org.example.dockerconfigmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerConfigMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerConfigMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dockerconfigmicroservice/DockerConfigMicroserviceApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package org.example.dockerconfigmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerConfigMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
